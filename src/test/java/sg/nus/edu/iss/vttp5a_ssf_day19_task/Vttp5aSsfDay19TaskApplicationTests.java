95fd5f9f793c85251387528a811917142bb41df7
remuslum <57741494+remuslum@users.noreply.github.com>
2024-12-06 13:41:03 +0800

    First commit

---
+++ src/test/java/sg/nus/edu/iss/vttp5a_ssf_day19_task/Vttp5aSsfDay19TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp5a_ssf_day19_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay19TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
