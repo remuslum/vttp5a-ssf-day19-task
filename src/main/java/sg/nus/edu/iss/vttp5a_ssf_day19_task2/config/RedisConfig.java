807d845ab04077e8a39849e63bc3a63484ebef0f
remuslum <57741494+remuslum@users.noreply.github.com>
2024-12-10 10:53:16 +0800

    Add second part

---
+++ src/main/java/sg/nus/edu/iss/vttp5a_ssf_day19_task2/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package sg.nus.edu.iss.vttp5a_ssf_day19_task2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import sg.nus.edu.iss.vttp5a_ssf_day19_task2.util.Constants;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private Integer redisPort;
+
+    @Value("${spring.data.redis.username}")
+    private String redisUsername;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        RedisStandaloneConfiguration rsc = new RedisStandaloneConfiguration();
+        rsc.setHostName(redisHost);
+        rsc.setPort(redisPort);
+        
+        if(redisUsername.trim().length() > 0){
+            rsc.setUsername(redisUsername);
+            rsc.setPassword(redisPassword);
+        }
+
+        JedisClientConfiguration jcc = JedisClientConfiguration.builder().build();
+        JedisConnectionFactory jcf = new JedisConnectionFactory(rsc, jcc);
+        jcf.afterPropertiesSet();
+
+        return jcf;
+    }
+
+    @Bean(Constants.TEMPLATE01)
+    public RedisTemplate<String, String> createRedisTemplate01(){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/sg/nus/edu/iss/vttp5a_ssf_day19_task2/util/Constants.java
@@ -0,0 +1,6 @@
+package sg.nus.edu.iss.vttp5a_ssf_day19_task2.util;
+
+public class Constants {
+    public static final String TEMPLATE01 = "template01";
+    public static final String REDISKEY = "products";
+}
